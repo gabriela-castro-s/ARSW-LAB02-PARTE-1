dd03d6571751c9ace3f6ff5f9905edc1e06ba762
carol <caroltatianacely@gmail.com>
2023-02-01 10:00:53 -0500

    Avance punto 1

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -5,6 +5,8 @@
  */
 package edu.eci.arsw.primefinder;
 
+import java.util.Scanner;
+
 /**
  *
  */
@@ -17,17 +19,18 @@ public class Control extends Thread {
     private final int NDATA = MAXVALUE / NTHREADS;
 
     private PrimeFinderThread pft[];
+    private Control control;
     
     private Control() {
         super();
         this.pft = new  PrimeFinderThread[NTHREADS];
-
+        control = this;
         int i;
         for(i = 0;i < NTHREADS - 1; i++) {
-            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA);
+            PrimeFinderThread elem = new PrimeFinderThread(i*NDATA, (i+1)*NDATA, this);
             pft[i] = elem;
         }
-        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1);
+        pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1, this);
     }
     
     public static Control newControl() {
@@ -36,9 +39,47 @@ public class Control extends Thread {
 
     @Override
     public void run() {
-        for(int i = 0;i < NTHREADS;i++ ) {
+        for (int i = 0; i < NTHREADS; i++) {
+            System.out.println("Inicio hilo");
             pft[i].start();
         }
+
+        while (true) {
+                try {
+                    sleep(TMILISECONDS);
+
+                    for (int i = 0; i < NTHREADS; i++) {
+                        pft[i].parar();
+                        System.out.println("Detiene hilos");
+                    }
+                    System.out.println("SE HAN ENCONTRADO: " + primosEncontrados() + " NUMEROS DE PRIMOS, presionar ENTER ");
+                    Scanner enter = new Scanner(System.in);
+                    enter.nextLine();
+
+                    for (int i = 0; i < NTHREADS; i++) {
+
+                        pft[i].correr();
+                        System.out.println("Corre hilos");
+                    }
+                    synchronized (control) {
+                        control.notifyAll();
+                    }
+
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+
+        private int primosEncontrados () {
+        int total = 0;
+        int i = 0;
+        while(i<NTHREADS){
+            if(pft[i].isWait()){
+                total = total+ pft[i].getprimosEncontrados();
+                i++;
+            }
+        }
+            return total;
+        }
     }
-    
-}
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -9,23 +9,38 @@ public class PrimeFinderThread extends Thread{
 	int a,b;
 	
 	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
+    private Control control;
+    boolean corre;
+	public PrimeFinderThread(int a, int b, Control control) {
 		super();
-                this.primes = new LinkedList<>();
+        corre = true;
+        this.primes = new LinkedList<>();
 		this.a = a;
 		this.b = b;
+        this.control = control;
+
 	}
 
         @Override
 	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
+            for (int i= a;i < b;i++){
+                if (!corre){
+                    try {
+                        synchronized (control){
+                            control.wait();
+
+                        }
+                    } catch (InterruptedException e){
+                        throw new RuntimeException(e);
+                    }
+                }
+                if(isPrime(i)){
                     primes.add(i);
                     System.out.println(i);
                 }
             }
-	}
+
+        }
 	
 	boolean isPrime(int n) {
 	    boolean ans;
@@ -43,5 +58,20 @@ public class PrimeFinderThread extends Thread{
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
+
+    public void parar() {
+        corre = false;
+    }
+
+    public void correr() {
+        corre = true;
+    }
+
+    public boolean isWait() {
+        return this.getState() == Thread.State.WAITING;
+    }
+
+    public int getprimosEncontrados() {
+        return primes.size();
+    }
 }
