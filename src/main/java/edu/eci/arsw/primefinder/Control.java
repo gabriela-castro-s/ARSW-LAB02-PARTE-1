bf1020a5e0f08324013d830f932e7ad24a53606e
Gabriela Castro <gabriela.castro@mail.escuelaing.edu.co>
2023-02-06 09:58:34 -0500

    Documentación y finalizacion de punto 1

--- src/main/java/edu/eci/arsw/primefinder/Control.java
+++ src/main/java/edu/eci/arsw/primefinder/Control.java
@@ -8,7 +8,7 @@ package edu.eci.arsw.primefinder;
 import java.util.Scanner;
 
 /**
- *
+ * Clase coordinadora de los hilos
  */
 public class Control extends Thread {
     
@@ -20,7 +20,12 @@ public class Control extends Thread {
 
     private PrimeFinderThread pft[];
     private Control control;
-    
+
+    private boolean over = true;
+
+    /**
+     * Constructor que crea los hilos a coordinar
+     */
     private Control() {
         super();
         this.pft = new  PrimeFinderThread[NTHREADS];
@@ -32,11 +37,18 @@ public class Control extends Thread {
         }
         pft[i] = new PrimeFinderThread(i*NDATA, MAXVALUE + 1, this);
     }
-    
+
+    /**
+     * Inicializa un nuevo coordinador
+     * @return coordinador de hilos
+     */
     public static Control newControl() {
         return new Control();
     }
 
+    /**
+     * Encargado de dar las instrucciones a los hilos
+     */
     @Override
     public void run() {
         for (int i = 0; i < NTHREADS; i++) {
@@ -46,21 +58,23 @@ public class Control extends Thread {
 
         while (true) {
                 try {
+                    //Después de cierto tiempo, se para el hilo
                     sleep(TMILISECONDS);
-
+                    //Debe cambiar el estado de todos los hilos
                     for (int i = 0; i < NTHREADS; i++) {
                         pft[i].parar();
-                        System.out.println("Detiene hilos");
+                        //System.out.println("Detiene hilos");
                     }
+                    //Indica el estado de los hilos y espera el enter para continuar la ejecución
                     System.out.println("SE HAN ENCONTRADO: " + primosEncontrados() + " NUMEROS DE PRIMOS, presionar ENTER ");
                     Scanner enter = new Scanner(System.in);
                     enter.nextLine();
-
+                    //Después de escuchar el enter, cambia el estado de los hilos paraque continuen su ejecución
                     for (int i = 0; i < NTHREADS; i++) {
-
                         pft[i].correr();
                         System.out.println("Corre hilos");
                     }
+                    //Cuando los estados cambian, se sincronizan los hilos y se les notifica que pueden continuar
                     synchronized (control) {
                         control.notifyAll();
                     }
@@ -68,10 +82,19 @@ public class Control extends Thread {
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                //Revisa si ya se terminó la ejecución de los hilos
+                if (over){
+                    break;
+                }
+
             }
         }
 
-        private int primosEncontrados () {
+    /**
+     * Calcula la cantidad de primos encontrados por todos los hilos
+     * @return total de primos
+     */
+    private int primosEncontrados () {
         int total = 0;
         int i = 0;
         while(i<NTHREADS){
@@ -81,5 +104,10 @@ public class Control extends Thread {
             }
         }
             return total;
-        }
     }
+
+    public void isOver(){
+        over = true;
+    }
+}
+
--- src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,14 +3,23 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Hilo encargado de la busqueda de primos
+ */
 public class PrimeFinderThread extends Thread{
 
 	
 	int a,b;
-	
 	private List<Integer> primes;
     private Control control;
     boolean corre;
+
+    /**
+     * Constructor de hilo encargado de buscar primos en un rango determinado
+     * @param a inicio de busqueda
+     * @param b fin de busqueda
+     * @param control coordinador de los hilos
+     */
 	public PrimeFinderThread(int a, int b, Control control) {
 		super();
         corre = true;
@@ -21,9 +30,14 @@ public class PrimeFinderThread extends Thread{
 
 	}
 
+    /**
+     * El método run se encarga de la ejecución del hilo
+     */
         @Override
 	public void run(){
-            for (int i= a;i < b;i++){
+            for (int i = a;i < b;i++){
+                //Revisa el estado del hilo, si está pausado, se sincroniza el control y se
+                //pide una espera para todos los hilos
                 if (!corre){
                     try {
                         synchronized (control){
@@ -34,15 +48,25 @@ public class PrimeFinderThread extends Thread{
                         throw new RuntimeException(e);
                     }
                 }
+                //De lo contrario, continua con la ejecución normal y revisa si su posición actual es primo
                 if(isPrime(i)){
                     primes.add(i);
                     System.out.println(i);
+                    //Revisa si ya termino de calcular los primos en su rango
+                    if (i == b){
+                        control.isOver();
+                    }
                 }
             }
 
         }
-	
-	boolean isPrime(int n) {
+
+    /**
+     * Revisa si un numero es primo
+     * @param n numero a revisar
+     * @return boolean resultante de la revisión
+     */
+    boolean isPrime(int n) {
 	    boolean ans;
             if (n > 2) { 
                 ans = n%2 != 0;
@@ -55,22 +79,32 @@ public class PrimeFinderThread extends Thread{
 	    return ans;
 	}
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-
+    /**
+     * Cambia el estado de ejecución del hilo a falso
+     */
     public void parar() {
         corre = false;
     }
 
+    /**
+     * Cambia el estado de ejecución del hilo a verdadero
+     */
     public void correr() {
         corre = true;
     }
 
+    /**
+     * Revisa el estado actual del hilo
+     * @return estado de ejecución del hilo
+     */
     public boolean isWait() {
         return this.getState() == Thread.State.WAITING;
     }
 
+    /**
+     * Retorna la cantidad de primos encontrados
+     * @return cantidad de números encontrados
+     */
     public int getprimosEncontrados() {
         return primes.size();
     }
